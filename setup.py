fec1c6fe9282337c1ba8d7e99b621ec9e1c297ac
ste <stephen.ofarrell@team.bumble.com>
2022-10-24 22:42:38 +0100

    fix description

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import setuptools
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setuptools.setup(
     name='lgtm-art',
     version='1.0.0',
     packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['opencv-python>=4.6.0.66'],
     entry_points={
         'console_scripts': "lgtm-art=lgtm:cli"
