ee542ffd22dcbfaf8281ddc9602cf7e05a217e51
ste <stephen.ofarrell@team.bumble.com>
2022-10-24 23:17:11 +0100

    circular import

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name='lgtm-art',
-    version='1.0.4',
+    version='1.0.5',
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=['opencv-python>=4.6.0.66'],
     entry_points={
-        'console_scripts': "lgtm-art=lgtm.lgtm:cli"
+        'console_scripts': "lgtm-art=lgtm_art.lgtm:cli"
     },
-    download_url='https://github.com/Steeeephen/LGTM-Art/archive/refs/tags/1.0.4.tar.gz'
+    download_url='https://github.com/Steeeephen/LGTM-Art/archive/refs/tags/1.0.5.tar.gz'
 )
