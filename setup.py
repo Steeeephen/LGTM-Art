36de2ada9ab8524e22ec6df399b1b05f13076163
ste <stephen.ofarrell@team.bumble.com>
2022-10-24 23:13:27 +0100

    abstract into directory

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name='lgtm-art',
-    version='1.0.3',
+    version='1.0.4',
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=['opencv-python>=4.6.0.66'],
     entry_points={
-        'console_scripts': "lgtm-art=lgtm:cli"
+        'console_scripts': "lgtm-art=lgtm.lgtm:cli"
     },
-    download_url='https://github.com/Steeeephen/LGTM-Art/archive/refs/tags/1.0.3.tar.gz'
+    download_url='https://github.com/Steeeephen/LGTM-Art/archive/refs/tags/1.0.4.tar.gz'
 )
